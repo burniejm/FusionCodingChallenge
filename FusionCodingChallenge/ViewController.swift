3891cc7081177dfe3f29ae662607a1808fdaac2a
John Macy <john@synapse.ws>
2023-01-20 15:35:43 -0600

    Initial Commit

---
+++ FusionCodingChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FusionCodingChallenge
+//
+//  Created by John Macy on 1/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
